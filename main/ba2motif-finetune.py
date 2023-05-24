5b33624e239d23604cc08544c69e990ddb9fe5b8
Esperanto-mega <57856207+Esperanto-mega@users.noreply.github.com>
2023-05-24 08:45:26 +0800

    Update ba2motif-finetune.py

--- main/ba2motif-finetune.py
+++ main/ba2motif-finetune.py
@@ -6,11 +6,6 @@ import os.path as osp
 import numpy as np
 
 machine = 'server'
-
-if machine != 'pc':
-    cur_path = '/home/data/yj/Bnova'
-    if cur_path not in sys.path:
-        sys.path.append(cur_path)
         
 import torch
 import torch.nn as nn
